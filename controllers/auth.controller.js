ca8732b514d9a8084102693b915828a1af8b4e26
Jose Maria Novillo <josenovillo88@gmail.com>
2023-09-12 22:01:40 -0300

    sign out + google signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,8 @@ import crypto from 'crypto'
 import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import User from '../models/user.js'
+import { verify } from '../helpers/google-verify.js'
+
 
 
 const controller = {
@@ -65,6 +67,69 @@ const controller = {
 
     },
 
+    googleSignin: async (req,res,next)=>{
+
+        const {token_id} = req.body
+
+        try {
+//verificamos el token de google que viene desde el front
+            const {name, email, image} = await verify (token_id)
+
+            let user = await User.findOne({email})
+//cuando el usuario no existe
+            if(!user){
+
+                //lo creamos
+                const data = {
+                    name,
+                    email,
+                    image,
+                    password:bcryptjs.hashSync(process.env.STANDARD_PASS, 10),
+                    google:true,
+                    verified_code:crypto.randomBytes(10).toString('hex')
+                }
+
+                user = await User.create(data)
+
+            }
+
+            //si el usuario existe lo logueo
+
+            user.online = true
+            await user.save()
+
+            const token =jwt.sign(
+                {
+                    id: user._id,
+                    email: user.email,
+                    name: user.name,
+                    image: user.picture
+                },
+                process.env.SECRET_TOKEN,
+                {expiresIn:'5h'}
+            )
+
+            return res.status(200).json({
+                success: true,
+                message:'User Logged Succesfully With Google',
+                response:{
+                    token,
+                    user:{
+                        name:user.name,
+                        email:user.email,
+                        image:user.image
+                    }
+                    
+                }
+            })
+            
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: 'Error: Failed to sign with Google'})
+        }
+    },
+
     signout : async (req,res,next)=>{
         try {
             const user = await User.findOneAndUpdate(
@@ -80,6 +145,23 @@ const controller = {
             res.status(500).json({
                 success: false,
                 message: 'Error: Failed to sign out user'})
+        }
+    },
+
+    token : async (req,res,next)=>{
+
+        const { user } = req
+
+        try {
+            return res.status(200).json({
+                user:{
+                    name:user.name,
+                    email:user.email,
+                    image:user.image
+                }
+            })
+        } catch (error) {
+            next(error)
         }
     }
 }
--- router/auth.router.js
+++ router/auth.router.js
@@ -10,16 +10,23 @@ import passport from '../middlewares/passport.js'
 
 
 
-const{signup, signin, signout} = authController
+const{signup, signin, signout, token, googleSignin} = authController
 
 const router = express.Router()
 
 router.post('/signup', accountExistsSignUp , signup )
+
 router.post('/signin', accountExistsSignIn, 
 accountHasBeenVerified, 
 passwordIsOk,
 signin )
+
+router.post('/google', googleSignin)
+
 router.post('/signout', passport.authenticate('jwt', { session: false}) ,signout )
+
+router.post('/token', passport.authenticate('jwt', { session: false}) ,token )
+
 
 
 
